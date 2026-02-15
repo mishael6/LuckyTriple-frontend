8b3e58ef238188d40fa6663352d72d107bd4332b
mishael6 <mishosmanmensah660@gmail.com>
2026-02-15 16:00:39 +0000

    tes

--- src/api-helper/index.js
+++ src/api-helper/index.js
@@ -14,7 +14,6 @@ axios.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-    config.headers['ngrok-skip-browser-warning'] = '69420';
   }
   return config;
 });
